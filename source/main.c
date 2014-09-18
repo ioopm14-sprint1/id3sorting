40de423dd241a2c6931a599f194064d8c3052b66
Tommy Vagbratt <tommy@vagbratt.se>
2014-09-18 12:09:29 +0200

    use new id3 interface

--- source/main.c
+++ source/main.c
@@ -3,15 +3,17 @@
 
 int main(int argc, const char *argv[])
 {
-  // ID3_read_file("data/music10.mp3");
-  struct ID3_file *file = ID3_open("data/music10.mp3");
-  struct ID3_data *data = NULL;
-  
-  while((data = ID3_read(file))){
-    printf("%s\n",data->data);
-    ID3_data_free(&data);
-   
-  }
-  ID3_close(&file);
-  return 0;
+    struct ID3_file *file = ID3_open("data/music10.mp3");
+    struct ID3_data *data = NULL;
+
+    printf("==================================\n");
+    while ((data = ID3_read(file))) {
+        printf("%s\n",data->data);
+        ID3_data_free(&data);
+    }
+    printf("==================================\n");
+
+    ID3_close(&file);
+
+    return 0;
 }
